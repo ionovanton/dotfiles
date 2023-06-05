29971c9dcc5ab0ad22369870d5c05fadf1bde7a0
Ionov Anton <manuvaad@gmail.com>
2023-06-05 23:58:41 +0300

    fixed plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -25,82 +25,6 @@ local luasnip = function(opts)
   })
 end
 
-local nvimtree_opts = {
-  filters = {
-    dotfiles = false,
-    exclude = { vim.fn.stdpath "config" .. "/lua/custom" },
-  },
-  disable_netrw = true,
-  hijack_netrw = true,
-  hijack_cursor = true,
-  hijack_unnamed_buffer_when_opening = false,
-  sync_root_with_cwd = true,
-  update_focused_file = {
-    enable = true,
-    update_root = false,
-  },
-  view = {
-    adaptive_size = false,
-    side = "left",
-    width = 30,
-    preserve_window_proportions = true,
-  },
-  git = {
-    enable = false,
-    ignore = true,
-  },
-  filesystem_watchers = {
-    enable = true,
-  },
-  actions = {
-    open_file = {
-      resize_window = true,
-    },
-  },
-  renderer = {
-    root_folder_label = false,
-    highlight_git = false,
-    highlight_opened_files = "none",
-
-    indent_markers = {
-      enable = false,
-    },
-
-    icons = {
-      show = {
-        file = true,
-        folder = true,
-        folder_arrow = true,
-        git = false,
-      },
-
-      glyphs = {
-        default = "󰈚",
-        symlink = "",
-        folder = {
-          default = "󰉋",
-          empty = "",
-          empty_open = "",
-          open = "",
-          symlink = "",
-          symlink_open = "",
-          arrow_open = "",
-          arrow_closed = "",
-        },
-        git = {
-          unstaged = "✗",
-          staged = "✓",
-          unmerged = "",
-          renamed = "➜",
-          untracked = "★",
-          deleted = "",
-          ignored = "◌",
-        },
-      },
-    },
-  },
-}
-
 local plugins = {
   {
     "hrsh7th/nvim-cmp",
@@ -151,14 +75,14 @@ local plugins = {
       require("cmp").setup(opts)
     end,
   },
-  -- {
-  --   "williamboman/mason.nvim",
-  --   opts = {
-  --     ensure_installed = {
-  --       "gopls",
-  --     },
-  --   },
-  -- },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+      },
+    },
+  },
   {
     "catppuccin/nvim",
     lazy = true,
@@ -213,8 +137,11 @@ local plugins = {
   },
   {
     'nvim-tree/nvim-tree.lua',
-    config = function()
-      require('nvim-tree').setup(nvimtree_opts)
+    opts = function()
+      return require "config.nvimtree"
+    end,
+    config = function(_, opts)
+      require('nvim-tree').setup(opts)
     end,
   },
   {
